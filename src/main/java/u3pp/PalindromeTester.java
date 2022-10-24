c5feb2bbfc4c9c8c4751603432d2645627426897
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2022-10-24 00:49:42 -0700

    Update PalindromeTester.java

---
+++ src/main/java/u3pp/PalindromeTester.java
@@ -0,0 +1,23 @@
+package u3pp;
+
+public class PalindromeTesster {
+
+    /**
+     * Tests whether a string is a palindrome. Case insensitive. 
+     * @param s  the string to test
+     * @return true if the string is a palindrome
+     */
+    public static boolean isPalindrome(String s) {
+        String reverse = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverse = reverse + s.charAt(i);
+        }
+        if (s.equals(rev)) {
+            return true;
+        }
+        return false;
+        
+    }
+}
+ 
